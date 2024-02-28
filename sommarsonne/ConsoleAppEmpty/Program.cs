1766d69a8e873a581aa9f9768e9a19cded59f5fd
sommarsonne <mumudynasty@gmail.com>
2024-02-28 21:15:12 +0300

    Second commit

---
+++ sommarsonne/ConsoleAppEmpty/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
