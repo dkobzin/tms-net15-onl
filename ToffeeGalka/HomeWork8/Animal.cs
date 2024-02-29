630eaf00cc589d2974d6f69ca0e6de39189c8754
ToffeeGalka <toffee@tut.by>
2024-02-29 15:08:39 +0300

    HomeWork8

---
+++ ToffeeGalka/HomeWork8/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+        public abstract void Eat();    
+    }
+}
