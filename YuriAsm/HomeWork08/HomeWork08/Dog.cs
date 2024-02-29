758343be27e51e81cd3a80438200dc4e5cd542af
Юрий <goker21@inbox.ru>
2024-02-29 21:18:29 +0300

    Add homeWork 8.1add homework 08

---
+++ YuriAsm/HomeWork08/HomeWork08/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HomeWork08
+{
+    public class Dog : Animal,IAnimalAction
+    {
+        public Dog(string name) : base(name)
+        {
+            this.Name = name;
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine($"Собака {Name} ест");
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine($"Собака {Name} гуяет");
+        }
+
+        public void Play(string joy)
+        {
+            Console.WriteLine($"Собака {Name} играет c {joy}");
+        }
+    }
+}
