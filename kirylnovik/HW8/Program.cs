42301b838cdc1e9c4aac632f1ad58fb27016e5c0
liweS <ka.novik@yandex.by>
2024-03-02 17:57:08 +0300

    Complete homework 8

--- kirylnovik/HW8/Program.cs
+++ kirylnovik/HW8/Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿namespace HW8;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Назовите свою собаку: ");
+        var dogName = Console.ReadLine();
+        
+        Console.Write("Назовите свою кошку: ");
+        var catName = Console.ReadLine();
+
+        Animal dog = new Dog();
+        dog.SetName(dogName);
+        
+        IAnimal cat = new Cat();
+        cat.SetName(catName);
+
+        dog.Eat();
+        cat.Eat();
+    }
+}
