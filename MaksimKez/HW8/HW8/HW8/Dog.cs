8363b07283fae126e31f8ff4aad0001af0909569
MaksimKez <prostomaxim765@gmail.com>
2024-03-03 23:28:18 +0300

    hw8KezMaksim

---
+++ MaksimKez/HW8/HW8/HW8/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HW8
+{
+    internal class Dog : Animal
+    {
+        internal override string Name { get; set; }
+        internal override void Eat() => Console.WriteLine($"Собака {Name} ест:)");
+        internal override string GetName() => Name;
+        internal override void SetName(string name) => Name = name;
+    }
+}
---
+++ MaksimKez/HW8/HW8/HW8/DogInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HW8
+{
+    internal class DogInterface : IHaveName, INameGettable, INameSettable, IEatable
+    {
+        public string Name { get; set; }
+
+        public void Eat() => Console.WriteLine($"Собака {Name} ест (интерфейс)");
+
+        public string GetName() => Name;
+
+        public void SetName(string name) => Name = name;
+    }
+}
