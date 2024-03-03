c3836b8ba5efee886b0a2b8007984f737f3f6a46
sommarsonne <mumudynasty@gmail.com>
2024-03-03 06:51:01 +0300

    Final

---
+++ sommarsonne/HomeworkLesson8/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkLesson8
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        
+        public abstract void Eat();
+        
+        public void GetName()
+        {
+            Console.WriteLine($"Собаку зовут: {Name}"); 
+        }
+
+
+
+    }
+}
