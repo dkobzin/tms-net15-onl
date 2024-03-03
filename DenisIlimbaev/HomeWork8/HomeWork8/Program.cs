7c518e912a4f329b8b49b12bea4da0b94e18a64c
Denisdeparture <126673275+Denisdeparture@users.noreply.github.com>
2024-03-03 17:16:11 +0300

    CommitHasHomeWork8 (#9)
    Add HomeWork8

--- DenisIlimbaev/HomeWork8/HomeWork8/Program.cs
+++ DenisIlimbaev/HomeWork8/HomeWork8/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace HomeWork8
+﻿using HomeTask8;
+class Work
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        try
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите имя собаки: ");
+            String? nameOfDog = Console.ReadLine();
+            if(String.IsNullOrEmpty(nameOfDog)) { throw new Exception("вы не присвоили имя"); }
+            Animal animal = new Dog(nameOfDog);
+            /*
+            или так:
+            Animal dog = new Dog() 
+            { 
+                Name = nameOfDog
+            };
+            */
+            animal.ActionAnimal("eat");
+            IAnimal animal1 = new Dog()
+            {
+                Name = nameOfDog
+            };
+            animal1.ActionAnimal("eat");
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 }
