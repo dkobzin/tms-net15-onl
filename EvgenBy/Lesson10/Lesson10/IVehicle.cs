bafc75371b414397413da70cea42267166f1e713
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-04 20:58:26 +0300

    Создал класс  IVehicle с двумя методами

---
+++ EvgenBy/Lesson10/Lesson10/IVehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    public interface IVehicle
+    {
+        void Drive(int distance);
+        bool Refuel(int amount);
+    }
+}
