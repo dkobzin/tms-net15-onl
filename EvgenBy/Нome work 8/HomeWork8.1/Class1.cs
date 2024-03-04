b8f8c9f328875698cc164e65ec0c44c390a591ce
Aleksandr Tkachev <r91tap@gmail.com>
2024-03-04 17:44:38 +0300

    дз8 с помощью интерфейсов

---
+++ EvgenBy/Нome work 8/HomeWork8.1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8._1
+{
+
+    internal class Dog : IAnimal
+    {
+        public string Name { get; set; }
+
+        public void Eat()
+        {
+            Console.Write("Ест мясо и ростительную пищу");
+        }
+    }
+}
---
+++ EvgenBy/Нome work 8/HomeWork8.1/Program.cs
@@ -0,0 +1,7 @@
+﻿using HomeWork8._1;
+
+Dog dog = new Dog();
+Console.WriteLine("Как зовут вашу собаку");
+dog.Name = Console.ReadLine();
+Console.Write(dog.Name + ' ');
+dog.Eat();
