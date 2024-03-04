e6fda44c01194d42b80901be4176ed7ff914578d
Denis <denis11you777602@gmail.com>
2024-03-04 21:00:38 +0300

    CommitDenis

---
+++ DenisIlimbaev/Lesson10HT9/HomeTask9/HomeTask9/Truck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenisHT9
+{
+    internal class Truck : Car
+    {
+        internal Truck(double fuel, double consumption) : base(fuel, consumption) { }
+
+    }
+}
