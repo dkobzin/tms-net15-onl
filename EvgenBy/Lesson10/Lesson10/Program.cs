c4e402ffc17bf01d753433e4f3418259e9406940
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-04 21:36:44 +0300

    Cделал основную логику в Main

--- EvgenBy/Lesson10/Lesson10/Program.cs
+++ EvgenBy/Lesson10/Lesson10/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Lesson10
+﻿using System.Text;
+
+namespace Lesson10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            var sportsCar = new SportsCar(0);
+            var truck = new Truck(0);
+
+            Console.WriteLine("Введите количество бензина для заправки");
+            int fuelToAdd = int.Parse(Console.ReadLine());
+
+            sportsCar.Refuel(fuelToAdd);
+            truck.Refuel(fuelToAdd);
+
+            sportsCar.Drive(50); // Можно изменить растояние
+            truck.Drive(30); //Можно изменить растояни
+
+            Console.ReadLine();
         }
     }
 }
