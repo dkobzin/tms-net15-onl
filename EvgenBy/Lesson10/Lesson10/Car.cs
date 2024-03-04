7a0614598e92697c2c2b9d64c19bbc70e747c823
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-04 21:17:52 +0300

    Создал абстрактный класс Car с конструктором, который получает параметр с начальным количеством топлива для автомобиля и его расход и реализует методы Drive и Refuel автомобиля.

---
+++ EvgenBy/Lesson10/Lesson10/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    public abstract class Car: IVehicle
+    {
+        private int _fuelLevel;
+        private readonly int _fuelConsumption;
+
+        public Car(int fuelLevel, int fuelConsumption)
+        {
+            _fuelLevel = fuelLevel;
+            _fuelConsumption = fuelConsumption;
+        }
+
+        public void Drive(int distance)
+        {
+            if (_fuelLevel > 0)
+            {
+                Console.WriteLine("Автомобиль движется.");
+                _fuelLevel -= distance * _fuelConsumption;
+
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно топлива для поездки");
+            }
+        }
+
+        public bool Refuel(int amount)
+        {
+            _fuelLevel += amount;
+            return true;
+        }
+    }
+}
