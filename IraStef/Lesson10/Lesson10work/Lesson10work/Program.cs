e9e042bd3041e72a58352d6e4e785c8c3138ca45
IraStefanova <iran.glare@gmail.com>
2024-03-05 16:46:55 +0300

    lesson10work

---
+++ IraStef/Lesson10/Lesson10work/Lesson10work/Program.cs
@@ -0,0 +1,13 @@
+﻿using Lesson10work;
+
+Console.WriteLine("Введите количество бензина для автомобиля SportCar:");
+int.TryParse(Console.ReadLine(), out var resultSportCar);
+IVehicle carSportCar = new SportCar(resultSportCar, 1);
+carSportCar.Drive();
+
+Console.WriteLine("Введите количество бензина для автомобиля Truck:");
+int.TryParse(Console.ReadLine(), out var resultTruck);
+IVehicle carTruck = new Truck(resultTruck, 2);
+carTruck.Drive();
+
+
---
+++ IraStef/Lesson10/Lesson10work/Lesson10work/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10work
+{
+    internal class Truck : Car
+    {
+        internal Truck(double fuel, double consumption) : base(fuel, consumption) { }
+        public override void Drive(int distance)
+        {
+            if (Fuel > 0) Console.WriteLine($"Автомобиль {GetType().Name} движется");
+            else Console.WriteLine($"Автомобиль {GetType().Name} стоит");
+        }
+        public override void Drive()
+        {
+            Drive(0);
+        }
+        public override bool Refuel(int gasoline)
+        {
+            Fuel += gasoline;
+            return true;
+        }
+    }
+}
