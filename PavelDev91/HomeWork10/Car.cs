84c2097c4eb16d7b68ac5deafea5e813fcfd2a45
PavelDev91 <paveldev1991@gmail.com>
2024-03-05 22:35:57 +0300

    (PR)PavelDev(HomeWork(10))

---
+++ PavelDev91/HomeWork10/Car.cs
@@ -0,0 +1,25 @@
+﻿
+//-------------------------------------------------------------
+abstract class Car : IVehicle
+{
+    public int fuelValue { get; set; } = 0;
+    public virtual void Drive(out string message)
+    {
+        if (fuelValue > 0)
+        {
+            message = "Ваш '" + this.ToString() + "' едет!";
+
+            return;
+        }
+
+        message = "У вашего автомобиля <" + this.ToString() + "> закончился бензин!";
+    }
+
+    public virtual bool ReFuel(int value)
+    {
+        fuelValue = value;
+
+        return true;
+    }
+}
+//-------------------------------------------------------------
