30a51584902c6a19a2e85e3892ca5ffad5374a1d
Dmitry Kobzin <dskobzin@hotmail.com>
2024-03-06 14:56:16 +0300

    Add sample lesson 10 task

---
+++ Samples/Lesson10/Lesson10Work/Lesson10Work/Truck.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson10Work;
+
+public class Truck(string name, int fuel, int consumption) : Car(name, fuel, consumption), IVehicle
+{
+    public bool isEnoughFuel => isEnoughFuelCar;
+
+    public void Drive(int distance = 0)
+    {
+        DriveCar(distance);
+        Console.WriteLine(isEnoughFuel ? $"{Name} is driven!" : $"{Name} have not enough fuel!");
+    }
+
+    public bool Refuel(int fuel)
+    {
+        RefuelCar(fuel);
+        Console.WriteLine($"{Name} is refueled!");
+        return isEnoughFuel;
+    }
+}
