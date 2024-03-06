2ba8f9fe085d87252d712fcc1516f3002f81a05c
Dmitry Kobzin <dskobzin@hotmail.com>
2024-03-06 14:09:00 +0300

    Add lambdas and attributes examples

---
+++ Samples/Lesson11/Lesson11Attributes/AgeValidationAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson11Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+public class AgeValidationAttribute : Attribute
+{
+    public int Age { get;}
+    public AgeValidationAttribute() { }
+    public AgeValidationAttribute(int age) => Age = age;
+}
