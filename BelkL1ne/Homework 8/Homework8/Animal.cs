12ff4b6a3d63d50e1f8499303ced3f916994d4de
Ivan Makhankov <6270103@gmail.com>
2024-03-08 12:32:26 +0300

    i.Makhankov-Homowork8 commit

---
+++ BelkL1ne/Homework 8/Homework8/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8Abstract
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public abstract void GetName();
+
+        public abstract string SetName();
+
+        public abstract void Eat();
+
+    }
+}
