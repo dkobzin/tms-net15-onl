0f5dd5bb8ec602ef576bd1754dd346fa794b4f54
Юрий <goker21@inbox.ru>
2024-03-11 00:02:39 +0300

    push a homework 10

---
+++ YuriAsm/Lesson10/Lesso10/SportCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    public class SportCar : Car
+    {
+   
+        public SportCar(  int fuel, int consumption) : base(fuel, consumption)
+        {
+           
+        }
+    }
+}
---
+++ YuriAsm/Lesson10/Lesso10/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    public class Truck : Car
+    {
+       
+        public Truck(int fuel, int consumption) : base(fuel, consumption)
+        {
+           
+        }
+    }
+}
