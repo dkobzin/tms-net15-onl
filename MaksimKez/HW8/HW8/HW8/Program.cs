9bd9c0e250a8ad895b1de30a877f1bd5175598fe
MaksimKez <156958833+MaksimKez@users.noreply.github.com>
2024-03-10 08:01:05 +0100

    Maksim kez hw8 (#28)
    Homework 08
    Co-authored-by: Dmitry Kobzin <dskobzin@hotmail.com>

--- MaksimKez/HW8/HW8/HW8/Program.cs
+++ MaksimKez/HW8/HW8/HW8/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Как назвать собаку?");
+            string dogName;
+            while (true)
+            {
+                dogName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(Console.ReadLine())) break;
+                Console.WriteLine("Введите кличку собаки корректно");
+            }
+
+            var absDog = new Dog();
+            var interDog = new DogInterface();
+
+            absDog.SetName(dogName);
+            interDog.SetName(dogName);
+
+            Console.WriteLine($"Кличка собаки: {absDog.GetName()}");
+            Console.WriteLine($"Кличка собаки(интерфейс): {interDog.GetName()}");
+
+            absDog.Eat();
+            interDog.Eat();
+
         }
     }
 }
