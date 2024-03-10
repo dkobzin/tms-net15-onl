813ff3d3e95b7b21c60aad6b3be2f396702608c1
Denis <denis11you777602@gmail.com>
2024-03-10 17:27:09 +0300

    CompletedHT11

---
+++ DenisIlimbaev/HomeTask11/Inventory.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask11
+{
+   
+    partial class Inventory<TProduct>  where TProduct : ProductStandart
+    {
+        private List<TProduct> listProduct;
+        internal event Action actionsin;
+        internal Inventory(params TProduct[] products)
+        {
+            listProduct = products.ToList();
+        }
+
+        internal double PriceInventory()
+        {
+            double totalPrice = 0;
+            foreach(var product in listProduct)
+            {
+                totalPrice += product.Price;
+            }
+            return totalPrice;
+        }
+        internal void AllProductInInventory()
+        {
+            foreach(var product in listProduct) Console.WriteLine($"имя продукта:{product.NameProduct} \nцена продукта: {product.Price}\nиндетификатор продукта: {product.Identity}\nколичество: {product.Quantity}\n ");
+            Console.WriteLine();
+        }
+        internal void DeletedProductInInventory(string ID)
+        {
+            Action action = () => Console.WriteLine("Удалён новый продукт");
+            actionsin += action;
+            listProduct = listProduct.Where(x => x.Identity != ID).ToList();
+        }
+        internal void AddProductInInventory(TProduct product)
+        {
+            Action action = () => Console.WriteLine("Добавлен новый продукт");
+            actionsin += action;
+            listProduct.Add(product);
+        }
+        internal bool OpovistatorEvent()
+        {
+            bool eventStart = false;
+            if (actionsin != null)
+            {
+                actionsin();
+                eventStart = true;
+                actionsin = null;
+            }
+            return eventStart;
+
+        }
+    }
+
+
+
+
+
+
+
+
+    partial class Inventory<TProduct> : IEnumerable 
+    {
+        public IEnumerator<(CategoryProduct, string, double)> GetEnumerator()
+        {
+            (CategoryProduct, string, double) container;
+            for (int i = 0; i < listProduct.Count; i++)
+            {
+                container = (listProduct[i].CategoryProduct, listProduct[i].NameProduct, listProduct[i].Price)!;
+                yield return container;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i  < listProduct.Count; i++)
+            {
+                yield return listProduct[i];
+            }
+        }
+    }
+
+}
---
+++ DenisIlimbaev/HomeTask11/UserInventory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask11
+{
+    class UserInventory<TProduct> where TProduct : ProductStandart, new()
+    {
+        internal Inventory<TProduct> UserInv {  get; private set; }
+        internal string[] arrayCategory {  get; private set; } =
+        {
+            "Еда",
+            "Косметика"
+        };
+        internal UserInventory(Inventory<TProduct> inventory)
+        {
+            UserInv = inventory;
+            
+        }
+
+        internal void Start()
+        {
+               
+            string[] actions = 
+            {
+                "1.Добавить продукт",
+                "2.Удалить продукт по ID",
+                "3.Вывести список всех продуктов",
+                "4.Получить сумму всех продуктов"
+            };
+            Console.WriteLine("Выберите действие: ");
+            foreach(string action in actions ) Console.WriteLine(action);
+            switch (Convert.ToUInt32(Console.ReadLine()))
+            {
+                case 1:
+                    UserInv.AddProductInInventory(new TProduct() { CategoryProduct = TakeMeCategory(), NameProduct = Input("Введите название продукта: "), Price = Convert.ToUInt32(Input("Введите цену на продукт: ")), Quantity = Convert.ToUInt32(Input("Введите количество продукта: ")) });
+                    break;
+                case 2:
+                    UserInv.DeletedProductInInventory(Input("Введите ID продукта: "));
+                break;
+                case 3:
+                    UserInv.AllProductInInventory();
+                break;
+                case 4:
+                    Console.WriteLine($"цена корзины на текущий момент: {UserInv.PriceInventory()}");
+                    break;
+                default:
+                    Console.WriteLine("unknown action");
+                    break;
+
+            }
+
+        }
+        private string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine() ?? throw new NullReferenceException();
+        }
+        private CategoryProduct TakeMeCategory()
+        {
+            for(int i = 0; i < arrayCategory.Length; i++) Console.WriteLine($"{i}.{arrayCategory[i]}");
+            Console.Write("Выберите категорию: ");
+            uint j = Convert.ToUInt32(Console.ReadLine());
+            return (CategoryProduct) ((j <= arrayCategory.Length) ? j : throw new Exception());
+        }
+         
+    }
+}
