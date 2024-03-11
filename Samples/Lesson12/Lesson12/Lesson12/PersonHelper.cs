b16de7118145629a07cd0ae0be91e34639ac7ba6
Dmitry Kobzin <dskobzin@hotmail.com>
2024-03-11 20:11:55 +0300

    Add additional Lesson 12

---
+++ Samples/Lesson12/Lesson12/Lesson12/PersonHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    internal class PersonHelper<T>(ICollection<Person> persons)
+        where T : ICollection<Person>
+    {
+        private ICollection<Person> Persons { get; } = persons 
+                                                       ?? throw new ArgumentNullException(nameof(persons));
+    }
+}
--- Samples/Lesson12/Lesson12/Lesson12/Program.cs
+++ Samples/Lesson12/Lesson12/Lesson12/Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine($"tomId: {tomId}");
 Console.WriteLine($"bobId: {bobId}");
 
 # region Generic Id
-/*PersonGeneric<int> tomGeneric = new PersonGeneric<int>(123, "Tom Generic");
+PersonGeneric<int> tomGeneric = new PersonGeneric<int>(123, "Tom Generic");
 PersonGeneric<string> bobGeneric = new PersonGeneric<string>("abc", "Bob Generic");
 //PersonGeneric<int> saraGeneric = new PersonGeneric<int>("abc", "Bob Generic"); // compilation error
 
@@ -22,47 +22,52 @@ int tomGenericId = tomGeneric.Id;
 string bobGenericId = bobGeneric.Id;
 
 Console.WriteLine($"tomGenericId: {tomGenericId}");
-Console.WriteLine($"bobGenericId: {bobGenericId}");*/
+Console.WriteLine($"bobGenericId: {bobGenericId}");
+var persons = new List<PersonGeneric<int>>()
+{
+    tomGeneric
+};
+var tomFind = persons.FirstOrDefault(_ => _.Id == 123);
 #endregion
 
 #region Generic Class
 
-/*Company<PersonGeneric<int>> company = new Company<PersonGeneric<int>>(tomGeneric);
-Console.WriteLine($"company CEO: {tomGeneric.Name} {tomGenericId}");*/
+Company<PersonGeneric<int>> company = new Company<PersonGeneric<int>>(tomGeneric);
+Console.WriteLine($"company CEO: {tomGeneric.Name} {tomGenericId}");
 
 #endregion
 
 #region Generic Multiparameters
 
-/*Director<int, string>  director = new(123, "email@company.com");
-Console.WriteLine($"Director: {director.Id} {director.Email}");*/
+Director<int, string>  director = new(123, "email@company.com");
+Console.WriteLine($"Director: {director.Id} {director.Email}");
 
 #endregion
 
 #region Generic extensions method
 
-/*var person = new PersonGeneric<string>("abc", "Person");
+var person = new PersonGeneric<string>("abc", "Person");
 var manager = Transformer.TransformToManager<PersonGeneric<string>, Manager<string>>(person, "person@company.com");
-Console.WriteLine($"Manager: {manager.Id} {manager.Name} {manager.Email}");*/
+Console.WriteLine($"Manager: {manager.Id} {manager.Name} {manager.Email}");
 
 #endregion
 
 #region Co / contr variation
 
 // Co
-/* Lesson12.Stack<Bear> bears = new Lesson12.Stack<Bear>();
+Lesson12.Stack<Bear> bears = new Lesson12.Stack<Bear>();
 bears.Push(new Bear("Grey"));
 
-// bears.Push(new Camel("Pale")); // compilation error
-// Lesson12.Stack<Animal> animals = bears; // compilation error
-// IPoppable<Animal> animals = bears;   // Legal
-// Animal a = animals.Pop();
-// Console.WriteLine(a.Name); */
+//bears.Push(new Camel("Pale")); // compilation error
+//Lesson12.Stack<Animal> animals = bears; // compilation error
+ IPoppable<Animal> animals = bears;   // Legal
+ Animal a = animals.Pop();
+ Console.WriteLine(a.Name); 
 
 // Contr
-/*IPushable<Animal> animals1 = new Lesson12.Stack<Animal>();
+IPushable<Animal> animals1 = new Lesson12.Stack<Animal>();
 IPushable<Bear> bears1 = animals1;    // Legal
-bears.Push (new Bear("Black"));*/
+bears.Push (new Bear("Black"));
  
 #endregion
 Console.ReadLine();
