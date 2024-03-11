08da9208e3c686ee81e5f091917c97d3ec700f75
Dorming0 <113914728+Dorming0@users.noreply.github.com>
2024-03-11 18:56:14 +0300

    Yuri asmukovich home work10 (#34)
    Homework 11

---
+++ YuriAsm/Lesson10/HomeWork10/Data/ExecutionManager.cs
@@ -0,0 +1,39 @@
+﻿namespace HomeWork10.Data
+{
+    public class ExecutionManager
+    {
+        private Func<IEnumerable<Service>> OnGetServices;
+
+        public ExecutionManager(Func<IEnumerable<Service>> onGetServices)
+        {
+            OnGetServices = onGetServices ?? throw new ArgumentNullException(nameof(onGetServices));
+        }
+
+        public void GetMaxPriceProducts()
+        {
+            var services = OnGetServices();
+
+            var maxPrice = services.Max(x => x.Price);
+
+            Console.WriteLine($"\nМаксимальное сумма - {maxPrice}\n");
+        }
+        public void GetMinPriceProducts()
+        {
+            var services = OnGetServices();
+
+            var minPrice = services.Min(x => x.Price);
+
+            Console.WriteLine($"\nМинимальная сумма - {minPrice}\n");
+        }
+        public void GetMiddlePriceProducts()
+        {
+            var services = OnGetServices();
+
+            var allPrice = services.Sum(x => x.Price);
+
+            var middlePrice = allPrice / services.Count();
+
+            Console.WriteLine($"\nСредняя стоимость товаров - {Math.Ceiling(middlePrice)}\n");
+        }
+    }
+}
