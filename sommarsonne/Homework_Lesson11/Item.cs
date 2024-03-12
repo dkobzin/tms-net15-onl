5aa6091101230c053641ec1f6d7cab37e8373067
Maxim M <157243145+sommarsonne@users.noreply.github.com>
2024-03-12 09:41:30 +0300

    M mukhin branch (#32)
    Home work 11 MMukhin
    Co-authored-by: Dmitry Kobzin <dskobzin@hotmail.com>

---
+++ sommarsonne/Homework_Lesson11/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lesson11
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int id { get; set; }
+
+        public void PrintItemInfo()
+        {
+            Console.WriteLine($"Name: {name}, Price: {price}, ID: {id}");
+        }
+    }
+}
