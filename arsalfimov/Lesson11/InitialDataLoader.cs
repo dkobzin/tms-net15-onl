ad9beeef7536b6a795e3411c2c9ef288b4b61703
Arsen <alfimov.ars@gmail.com>
2024-03-13 21:13:39 +0300

    Final 1.0

---
+++ arsalfimov/Lesson11/InitialDataLoader.cs
@@ -0,0 +1,49 @@
+﻿namespace Lesson11;
+class InitialDataLoader(string filename)
+{
+    private readonly string filename = filename;
+
+    public void LoadInitialData(Inventory inventory)
+    {
+        if (!File.Exists(filename))
+        {
+            AddInitialData(inventory);
+        }
+        else
+        {
+            FileInfo fileInfo = new(filename);
+            if (fileInfo.Length == 0)
+            {
+                AddInitialData(inventory);
+            }
+            else
+            {
+                inventory.LoadFromFile(filename);
+            }
+        }
+    }
+
+    private void AddInitialData(Inventory inventory)
+    {
+        List<Product> initialProducts =
+        [
+            new Product("Table", 12.75, 25, Guid.NewGuid()),
+            new Product("Bed", 30.00, 10, Guid.NewGuid()),
+            new Product("Desk", 18.50, 20, Guid.NewGuid()),
+            new Product("Lamp", 8.75, 50, Guid.NewGuid()),
+            new Product("Bookshelf", 22.00, 15, Guid.NewGuid()),
+            new Product("TV Stand", 45.25, 5, Guid.NewGuid()),
+            new Product("Coffee Table", 25.00, 30, Guid.NewGuid()),
+            new Product("Dining Table", 60.25, 8, Guid.NewGuid()),
+            new Product("Nightstand", 17.75, 12, Guid.NewGuid()),
+            new Product("Cabinet", 40.50, 18, Guid.NewGuid())
+        ];
+
+        foreach (var product in initialProducts)
+        {
+            inventory.AddProduct(product);
+        }
+
+        inventory.SaveToFile(filename);
+    }
+}
---
+++ arsalfimov/Lesson11/MenuHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Lesson11;
+
+public static class MenuHandler
+{
+    public static string ShowMainMenuAndGetChoice()
+    {
+        PrintMainMenu();
+
+        Console.Write("Select an action: ");
+        return Console.ReadLine();
+    }
+
+    private static void PrintMainMenu()
+    {
+        Console.WriteLine("\nSelection Menu:\n");
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("1. Add a new product");
+        Console.WriteLine("2. Delete a product by ID");
+        Console.WriteLine("3. Display a list of all products");
+        Console.WriteLine("4. Get the sum of all products");
+        Console.WriteLine("5. Get the maximum product price");
+        Console.WriteLine("6. Get the minimum product price");
+        Console.WriteLine("7. Get the average price of the products");
+        Console.ResetColor();
+
+        string separator = new('-', 40);
+        Console.WriteLine(separator);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("8. Save changes to a file");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("9. Exit the program\n");
+        Console.ResetColor();
+    }
+}
+
