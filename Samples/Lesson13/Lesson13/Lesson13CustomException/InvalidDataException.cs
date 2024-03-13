35d2007534a7883d0b4d9a7ba5aa5340fca39633
Dmitry Kobzin <dskobzin@hotmail.com>
2024-03-13 13:09:29 +0300

    Add samples for Lesson13

---
+++ Samples/Lesson13/Lesson13/Lesson13CustomException/InvalidDataException.cs
@@ -0,0 +1,20 @@
+﻿namespace Lesson13CustomException;
+
+public class InvalidDataException : Exception
+{
+    public ExceptionStatusEnum StatusEnum { get; protected set;}
+
+    public InvalidDataException()
+    {
+    }
+
+    public InvalidDataException(ExceptionStatusEnum statusEnum)
+    {
+        StatusEnum = statusEnum;
+    }
+
+    public InvalidDataException(string? message, ExceptionStatusEnum statusEnum) : base(message)
+    {
+        StatusEnum = statusEnum;
+    }
+}
