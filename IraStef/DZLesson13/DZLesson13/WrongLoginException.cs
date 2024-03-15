1226ef5c8b883409c626dc685a932b7d9db7a1f4
IraStefanova <iran.glare@gmail.com>
2024-03-15 16:31:48 +0300

    Ira Stefanova DZLesson13

---
+++ IraStef/DZLesson13/DZLesson13/WrongLoginException.cs
@@ -0,0 +1,10 @@
+﻿namespace DZLesson13
+{
+    public class WrongLoginException : Exception 
+    {
+        public WrongLoginException(string message)
+            : base(message) { }
+        public WrongLoginException()
+            : base() { }
+    }  
+}
