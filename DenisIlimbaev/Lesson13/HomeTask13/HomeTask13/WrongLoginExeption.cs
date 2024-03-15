af705b68be87ed302cdc3ec6da33365522f290ee
Denis <denis11you777602@gmail.com>
2024-03-15 18:59:01 +0300

    CompletetdHT13

---
+++ DenisIlimbaev/Lesson13/HomeTask13/HomeTask13/WrongLoginExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask13
+{
+    internal class WrongLoginExeption : Exception
+    {
+        internal WrongLoginExeption(string ms) : base(ms) { }
+        internal WrongLoginExeption() { }
+    }
+
+}
+
