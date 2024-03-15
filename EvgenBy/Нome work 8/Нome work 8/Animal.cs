56543e6c0584cafaf60c14a6114e9bd8ed49f73d
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-15 09:04:36 +0300

    Krepskiy.E.Homework8 (#26)
    Finished my homework 8  with implimentation of the inteface 
    
    Co-authored-by: Dmitry Kobzin <dskobzin@hotmail.com>

---
+++ EvgenBy/Нome work 8/Нome work 8/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Нome_work_8
+{
+    public abstract class Animal
+    {
+        // Свойство Name
+        public string Name { get; set; }
+
+        // Метод для установки имени
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        // Метод для получения имени
+        public string GetName()
+        {
+            return Name;
+        }
+
+        // Абстрактный метод для еды
+        public abstract void Eat();
+    }
+}
