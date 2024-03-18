50f08283469af38f3f3bdccc44deecac4b4e9ff9
IraStefanova <157607035+IraStefanova@users.noreply.github.com>
2024-03-18 09:18:33 +0300

    Ira stefanova dz lesson13 (#41)
    * Ira Stefanova DZLesson13
    Co-authored-by: Dmitry Kobzin <dskobzin@hotmail.com>

---
+++ IraStef/DZLesson13/DZLesson13/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace DZLesson13
+{
+    internal class Program
+    {
+        public static bool Login(User user)
+        {
+            if (user == null)
+            { throw new ArgumentNullException(); }
+            if (user.Login.Length >= 20 || user.Login.Length != user.Login.Replace(" ", "").Length)
+            {
+                throw new WrongLoginException("Login is wrong!");
+            }
+            if (user.Password.Length >= 20 ||
+                user.Password.Length != user.Password.Replace(" ", "").Length ||
+                !user.Password.Any(char.IsDigit))
+            {
+                throw new WrongPasswordException("Password is wrong!");
+            }
+            if (user.Password != user.ConfirmPassword)
+            {
+                throw new WrongPasswordException("Confirm password is wrong!");
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter login");
+            string login = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Confirm password:");
+            string confirmPassword = Console.ReadLine();
+
+            User user = new(login, password, confirmPassword);
+
+            try
+            {
+                Login(user);
+                Console.WriteLine("OK");
+            }
+            catch (WrongLoginException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (WrongPasswordException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ IraStef/DZLesson13/DZLesson13/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZLesson13
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public User(string login, string password, string confirmPassword) 
+        {
+            Login = login;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+        }
+    }
+}
