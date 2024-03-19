23d1cd3ab3f5b300689ec61d11430cf7797082c6
MaksimKez <prostomaxim765@gmail.com>
2024-03-19 18:14:29 +0100

    made with error
    error IOException

---
+++ MaksimKez/HW14/HW14/HW14/InvalidPathEnteredException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    internal class InvalidPathEnteredException : Exception
+    {
+        public InvalidPathEnteredException()
+            : base() { }
+        public InvalidPathEnteredException(string message) 
+            : base(message) { }
+    }
+}
---
+++ MaksimKez/HW14/HW14/HW14/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HW14
+{
+    [Serializable]
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        [JsonIgnore]
+        public string IDCard { get; set; }
+        public User(string name, string surname, int age, string iDCard)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            IDCard = iDCard;
+        }
+    }
+}
