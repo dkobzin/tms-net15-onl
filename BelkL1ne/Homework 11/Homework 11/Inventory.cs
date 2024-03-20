a69b23f2b869ebd9a58bda55173208ff322c4cf7
BelkL1ne <156948945+BelkL1ne@users.noreply.github.com>
2024-03-20 18:48:27 +0300

    Homework 11 (#52)
    Homework 11

---
+++ BelkL1ne/Homework 11/Homework 11/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+    internal class Inventory ()
+    {
+        public void GetName(List<Product> products)
+        {
+            foreach (var productNames in products)
+            {
+                Console.WriteLine(productNames.Name);
+            }
+        }
+        public void DeleteProduct(List<Product> products)
+        {
+            Console.WriteLine("Введите id позции, которую хотите удалить.");
+            int positionId = Convert.ToInt32(Console.ReadLine()) + 1;
+            products.RemoveAt(positionId);
+        }
+        public void AddProduct (List<Product> products)
+        {
+            int productId = 1;
+
+            Console.WriteLine("Введите название, цену и количество продукта");
+            string? name = Console.ReadLine();
+            int price = Convert.ToInt32(Console.ReadLine());
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            Product product = new Product(name, price, productId++, amount);
+            products.Add(product);
+        }
+    }
+}
---
+++ BelkL1ne/Homework 11/Homework 11/Program.cs
@@ -0,0 +1,51 @@
+﻿using Homework_7;
+using System.Collections.Generic;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void DisplayMessage(string message) => Console.WriteLine(message);
+        Inventory inventory = new Inventory();
+        ExecutionManager executionManager = new ExecutionManager();
+        executionManager.Notify += DisplayMessage;
+        List<Product> products = new List<Product>();
+        while (true)
+        {
+            Console.WriteLine("1 - Add product, 2 - Delete product by ID," +
+            "3 - Display list of all products, 4 - Get sum of all products," +
+            "5 - Get product with the highest price, 6 - Get product with the highest price," +
+            "7 - Get average price of all products, 8 - Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    inventory.AddProduct(products);
+                    break;
+                case 2:
+                    inventory.DeleteProduct(products);
+                    break;
+                case 3:
+                    inventory.GetName(products);
+                    break;
+                case 4:
+                    executionManager.GetSum(products);
+                    break;
+                case 5:
+                    executionManager.GetMaxPrice(products);
+                    break;
+                case 6:
+                    executionManager.GetMinPrice(products);
+                    break;
+                case 7:
+                    executionManager.GetAveragePrice(products);
+                    break;
+                case 8:
+                    Environment.Exit(0);
+                    break;
+                default: Console.WriteLine("Invalid input"); break;
+            }
+        }
+    }
+}
