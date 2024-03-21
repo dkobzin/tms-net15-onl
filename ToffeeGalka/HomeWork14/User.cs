1a9080041e38fe13ac6f981b60cf828fedd372a7
ToffeeGalka <toffee@tut.by>
2024-03-21 16:10:56 +0300

    HomeWork14

---
+++ ToffeeGalka/HomeWork14/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HomeWork14
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [JsonIgnore]
+        public int? IDCard;
+        public User()
+        {
+        }
+        public User(string firstName, string lastName, int age, int iDCard)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IDCard = iDCard;
+        }
+    }
+}
