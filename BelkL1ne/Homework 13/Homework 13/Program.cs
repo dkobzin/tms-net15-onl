97c66cdb0f09049483d4b68b4a493613dcca2e2d
Ivan Makhankov <6270103@gmail.com>
2024-03-24 19:17:37 +0300

    Homework 13

---
+++ BelkL1ne/Homework 13/Homework 13/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                User user = new User("BelkL1ne", "Cartoon12");
+                Console.WriteLine(Login(user));
+                if (!Login(user)) { throw new WrongPasswordException("Invalid password"); }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static bool Login(User user)
+        {
+            if (user.Password != user.ConfirmPassword)
+            {
+                return false;
+                
+            }
+            return true;
+        }
+    }
+}
---
+++ BelkL1ne/Homework 13/Homework 13/WrongLoginException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_13
+{
+    internal class WrongLoginException : Exception
+    {
+        public WrongLoginException(string message) : base(message) { }
+    }
+}
