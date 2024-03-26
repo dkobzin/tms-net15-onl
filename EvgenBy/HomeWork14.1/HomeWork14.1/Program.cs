9606e2e9f85e043a62d235edf77d0a1d69427cd5
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-26 14:32:52 +0300

    First commit.

---
+++ EvgenBy/HomeWork14.1/HomeWork14.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ EvgenBy/HomeWork14.1/HomeWork14.1/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14._1
+{
+    internal class User
+    {
+    }
+}
