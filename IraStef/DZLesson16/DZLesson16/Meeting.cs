c8eba67dee66629fb56d8899cc30e836d1a1d380
IraStefanova <157607035+IraStefanova@users.noreply.github.com>
2024-03-26 16:17:27 +0300

    Ira Stefanova dz lesson16 (#65)
    * IraStefanova-DZLesson16
    
    * IraStefanova-DZLesson16

---
+++ IraStef/DZLesson16/DZLesson16/Meeting.cs
@@ -0,0 +1,9 @@
+﻿namespace DZLesson16
+{
+    public class Meeting
+    {
+        public int PeopleCount { get; set; }
+        public TimeSpan  MeetingMaxTime { get; set; }
+
+    }
+}
---
+++ IraStef/DZLesson16/DZLesson16/MeetingService.cs
@@ -0,0 +1,10 @@
+﻿namespace DZLesson16
+{
+    public class MeetingService: IMeetingService
+    {
+        public Meeting GetMeeting()
+        {
+            return new Meeting() { PeopleCount = 5, MeetingMaxTime = new TimeSpan(1, 30, 0) };
+        }
+    }
+}
