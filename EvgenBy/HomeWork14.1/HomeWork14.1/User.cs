9f6109b3d5487dfefe5d4638154ec90493d8a6e1
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-26 14:35:41 +0300

    Сделал юзер.

--- EvgenBy/HomeWork14.1/HomeWork14.1/User.cs
+++ EvgenBy/HomeWork14.1/HomeWork14.1/User.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
-namespace HomeWork14._1
+
+
+namespace HomeWork14
 {
-    internal class User
+    public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [JsonIgnore] public int IDCard { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string firstName, string lastName, int age, int idCard)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IDCard = idCard;
+        }
+
     }
 }
