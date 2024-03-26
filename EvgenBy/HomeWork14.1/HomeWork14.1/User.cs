b7b28db283fae1c80b8f8584133124181315f93c
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-03-26 16:19:31 +0300

    KrepskiyHomeWork14 (#66)
    * First commit.
    
    * Сделал юзер.
    
    * Добавли осн. логику.
    
    * Добавил json

---
+++ EvgenBy/HomeWork14.1/HomeWork14.1/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.Json.Serialization;
+
+
+
+namespace HomeWork14
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [JsonIgnore] public int IDCard { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string firstName, string lastName, int age, int idCard)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IDCard = idCard;
+        }
+
+    }
+}
