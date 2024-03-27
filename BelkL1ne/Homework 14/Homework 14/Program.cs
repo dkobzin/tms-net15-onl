5b148c357b22ef3819e81a79743e5c6ed2bd75d6
Ivan Makhankov <6270103@gmail.com>
2024-03-27 21:58:38 +0300

    I.Makhankov-Homework 14

---
+++ BelkL1ne/Homework 14/Homework 14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System;
+using System.Reflection.Metadata;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Homework_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter path, where json file.");
+                string? path = @"C:\SomeFolder\"; //Convert.ToString(Console.ReadLine());
+
+                if (path == null || !Directory.Exists(path))
+                { throw new FolderException("This folder doesn't exist."); }
+
+                string[] files = Directory.GetFiles(path, "*.json");
+                foreach (string file in files) { Console.WriteLine(file); }
+
+                if (files.Length == 0) 
+                { throw new FolderException("This folder doesn't contains any of json files."); }
+                else if (files.Length > 1)
+                { throw new FolderException("This folder contains too many of json files."); }
+
+                FileStream fsJson = new FileStream(files[0], FileMode.OpenOrCreate);
+                
+                User? billy = JsonSerializer.Deserialize<User>(fsJson);
+                Console.WriteLine($"FirstName: {billy?.FirstName}  LastName: {billy?.LastName}" +
+                $" Age: {billy?.Age} Idcard: {billy?.IDCard}");
+                Console.WriteLine("Object has been deserialized from json");
+
+                FileStream fsXml = new FileStream(path + $"user_{billy?.FirstName}_{billy?.LastName}.xml", FileMode.OpenOrCreate);
+
+                XmlSerializer xmlSer = new XmlSerializer(typeof(User));
+                xmlSer.Serialize(fsXml, billy);
+                Console.WriteLine("Object has been serialized into xml");
+            }
+            catch (FolderException ex) { Console.WriteLine(ex.Message); }
+            finally { }
+        }
+    }
+}
---
+++ BelkL1ne/Homework 14/Homework 14/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace Homework_14
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        [JsonIgnore]
+        public string IDCard { get; set; }
+        public User(string firstName, string lastName, string age, string idCard)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IDCard = idCard;
+        }
+        public User() { }
+
+    }
+}
