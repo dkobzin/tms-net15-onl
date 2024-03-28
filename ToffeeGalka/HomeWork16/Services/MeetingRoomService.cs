863a9d87f67a33029682e5f953abd6e886823cad
ToffeeGalka <toffee@tut.by>
2024-03-28 14:32:28 +0300

    HomeWork16

---
+++ ToffeeGalka/HomeWork16/Services/MeetingRoomService.cs
@@ -0,0 +1,12 @@
+﻿using HomeWork16.Models;
+
+namespace HomeWork16.Services
+{
+    public class MeetingRoomService : IMeetingRoomService
+    {
+        public MeetingRoom GetMeeting() 
+        {
+        return new MeetingRoom() { MaxPeople = 10, MaxTime = new TimeSpan (2, 45, 0) };
+        }
+    }
+}
