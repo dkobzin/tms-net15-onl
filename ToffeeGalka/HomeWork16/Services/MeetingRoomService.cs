d15556ded59f206fb70ddba6a582fa4e0d2839b9
ToffeeGalka <157503739+ToffeeGalka@users.noreply.github.com>
2024-03-29 13:19:29 +0300

    Toffee galka home work 16 (#70)
    HomeWork16

---
+++ ToffeeGalka/HomeWork16/Services/MeetingRoomService.cs
@@ -0,0 +1,19 @@
+﻿using HomeWork16.Models;
+
+namespace HomeWork16.Services
+{
+    public class MeetingRoomService : IMeetingRoomService
+    {
+        private readonly IConfiguration _configuration;
+        public MeetingRoomService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public MeetingRoom GetMeeting() 
+        {
+            var peoples = _configuration.GetValue<int>("MaxPeople");
+            var durationMinutes = _configuration.GetValue<int>("MaxTime");
+        return new MeetingRoom() { MaxPeople = peoples, MaxTime = TimeSpan.FromMinutes(durationMinutes) };
+        }
+    }
+}
