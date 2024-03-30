15963a77dd841cc0e86612f47080bd513e604b75
Arsen <alfimov.ars@gmail.com>
2024-03-30 15:42:40 +0300

    Added Lesson16

---
+++ arsalfimov/Lesson16/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson16.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
