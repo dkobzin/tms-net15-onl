3afe43093836afe8bfc7cbac79322ac497ee5c7d
Arsen <alfimov.ars@gmail.com>
2024-03-30 15:55:42 +0300

    arsalfimov-Lesson16-final

---
+++ arsalfimov/Lesson16/Controllers/MeetingRoomController.cs
@@ -0,0 +1,17 @@
+﻿using Lesson16.Services;
+using Lesson16.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson16.Controllers
+{
+    public class MeetingRoomController(IMeetingRoomSettingsService settingsService) : Controller
+    {
+        private readonly IMeetingRoomSettingsService _settingsService = settingsService;
+
+        public IActionResult Index()
+        {
+            var settings = _settingsService.GetMeetingRoomSettings();
+            return View(settings);
+        }
+    }
+}
