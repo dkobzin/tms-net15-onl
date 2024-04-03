eb03d0343c90e27e5ce8ff3ca405a1901adb2b9b
ToffeeGalka <toffee@tut.by>
2024-04-03 13:58:27 +0300

    HomeWork17

---
+++ ToffeeGalka/HomeWork17/Services/MeetingRoomService.cs
@@ -0,0 +1,35 @@
+﻿using HomeWork17.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Configuration;
+
+namespace HomeWork17.Services
+{
+    public class MeetingRoomService : IMeetingRoomService
+    {
+        private readonly IConfiguration _configuration;
+        public MeetingRoomService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public bool EditRoom(MeetingRoomEdit roomData)
+        {
+            try
+            {
+                _configuration["MaxPeople"] = roomData.MaxPeople.ToString();
+                _configuration["MaxTime"] = roomData.MaxTime.ToString();
+                 return  true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+        public MeetingRoom GetMeeting() 
+        {
+            var peoples = _configuration.GetValue<int>("MaxPeople");
+            var durationMinutes = _configuration.GetValue<int>("MaxTime");
+            return new MeetingRoom() { MaxPeople = peoples, MaxTime = TimeSpan.FromMinutes(durationMinutes) };
+        }
+
+    }
+}
