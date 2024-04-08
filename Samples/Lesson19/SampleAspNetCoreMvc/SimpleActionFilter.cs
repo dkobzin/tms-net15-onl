a5b6c49e0980ca64755c14b155c83fa14f6e67ee
Dmitry Kobzin <dskobzin@hotmail.com>
2024-04-08 22:31:26 +0300

    Sample to Lesson19

---
+++ Samples/Lesson19/SampleAspNetCoreMvc/SimpleActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SampleAspNetCoreMvc;
+
+public class SimpleActionFilter: Attribute, IActionFilter // IResourceFilter //, IResultFilter, IExceptionFilter, IAuthorizationFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        //context.HttpContext.Response.WriteAsync($"{nameof(OnActionExecuting)}");
+        Console.WriteLine($"{context.Controller.ToString()}.{nameof(OnActionExecuting)}");
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        //context.HttpContext.Response.WriteAsync($"{nameof(OnActionExecuted)}");
+        Console.WriteLine($"{context.Controller.ToString()}.{nameof(OnActionExecuted)}");
+    }
+}
