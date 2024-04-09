7858f21139a6254edb43fc576cfdb74f96c56bb2
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:12:08 +0300

    Add a Inventory

--- EvgenBy/Home_work11/Home_work11/Inventory.cs
+++ EvgenBy/Home_work11/Home_work11/Inventory.cs
@@ -8,5 +8,51 @@ namespace Home_work11
 {
     internal class Inventory
     {
+        private List<Product> products;
+
+        public Inventory()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public decimal GetPrice()
+        {
+            decimal price = 0;
+
+            foreach (var product in products)
+            {
+                price += product.Price * product.Quantity;
+            }
+            return price;
+        }
+
+        public decimal GetMaxPrice()
+        {
+            if (products.Count == 0)
+                return 0;
+
+            return products.Max(p => p.Price);
+        }
+
+        public decimal GetMinPrice()
+        {
+            if (products.Count == 0)
+                return 0;
+
+            return products.Min(p => p.Price);
+        }
+
+        public decimal GetAvgPrice()
+        {
+            if (products.Count == 0)
+                return 0;
+
+            return products.Average(p => p.Price);
+        }
     }
 }
