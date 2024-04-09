a94a74a5696baafd36701498ba3350f5c0fe7e99
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:13:17 +0300

    Add a product

--- EvgenBy/Home_work11/Home_work11/Product.cs
+++ EvgenBy/Home_work11/Home_work11/Product.cs
@@ -8,5 +8,18 @@ namespace Home_work11
 {
     internal class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+
+        public Product(int id, string name, decimal price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
     }
 }
