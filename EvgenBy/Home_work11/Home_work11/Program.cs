27aca9184eab0df2561ccee16e7f2516f7f7325c
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:15:19 +0300

    corrected a mistake

--- EvgenBy/Home_work11/Home_work11/Program.cs
+++ EvgenBy/Home_work11/Home_work11/Program.cs
@@ -5,18 +5,19 @@
         static void Main(string[] args)
         {
 
-            
+
             Product product1 = new Product(1, "Product 1", 10.99m, 5);
             Product product2 = new Product(2, "Product 2", 5.99m, 10);
 
-            
+
             Inventory inventory = new Inventory();
 
-            
+
             inventory.AddProduct(product1);
             inventory.AddProduct(product2);
 
-            
+
             ExecutionManager.DoInventory(inventory);
         }
+    }
 }
