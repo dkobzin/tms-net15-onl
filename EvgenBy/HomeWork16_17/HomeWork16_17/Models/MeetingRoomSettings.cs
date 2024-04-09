f6d26a7871731309188e286666a67c4a7e612bbf
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:50:19 +0300

    add model

---
+++ EvgenBy/HomeWork16_17/HomeWork16_17/Models/MeetingRoomSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeWork16_17.Models
+{
+    public class MeetingRoomSettings
+    {
+        public string MeetingRoomName { get; set; }
+        public int MaxPeople { get; set; }
+        public TimeSpan MaxMeetingDuration { get; set; }
+    }
+
