27126b890903937bd6a027938a3f7352ae668c3a
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:49:09 +0300

    Add controller

---
+++ EvgenBy/HomeWork16_17/HomeWork16_17/Controllers/MeetingRoomController.cs
@@ -0,0 +1,33 @@
+﻿using HomeWork16_17.Models;
+using HomeWork16_17.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork16_17.Controllers
+{
+    public class MeetingRoomController : Controller
+    {
+        private readonly IMeetingRoomSettingsService _settingsService;
+
+        public MeetingRoomController(IMeetingRoomSettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
+
+        public IActionResult Index(string meetingRoomName)
+        {
+            var settings = _settingsService.GetSettings(meetingRoomName);
+            return View(settings);
+        }
+
+        [HttpPost]
+        public IActionResult ModifySettings(string meetingRoomName, MeetingRoomSettings newSettings)
+        {
+            if (ModelState.IsValid)
+            {
+                _settingsService.SaveSettings(meetingRoomName, newSettings);
+                return RedirectToAction("Index", new { meetingRoomName });
+            }
+            return View("Index", newSettings);
+        }
+    }
+}
