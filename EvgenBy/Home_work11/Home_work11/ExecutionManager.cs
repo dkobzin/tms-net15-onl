44c5e5f719bbdda037d8c5010447a486cacd69af
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:09:34 +0300

    Add classes

---
+++ EvgenBy/Home_work11/Home_work11/ExecutionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work11
+{
+    internal class ExecutionManager
+    {
+    }
+}
---
+++ EvgenBy/Home_work11/Home_work11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Home_work11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
