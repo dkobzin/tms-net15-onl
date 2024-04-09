99d99a790d91d769e7e46a17e887f1d90542aa3d
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:37:40 +0300

    First commit

---
+++ EvgenBy/HomeWork16_17/HomeWork16_17/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using HomeWork16_17.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HomeWork16_17.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EvgenBy/HomeWork16_17/HomeWork16_17/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeWork16_17.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
