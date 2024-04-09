74b797f162af7f0689a662c90b7c55994aaa483a
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 19:18:34 +0300

    Add programm

--- EvgenBy/HomeWork16_17/HomeWork16_17/Program.cs
+++ EvgenBy/HomeWork16_17/HomeWork16_17/Program.cs
@@ -1,3 +1,6 @@
+using HomeWork16.Services;
+using HomeWork16_17.Services;
+
 namespace HomeWork16_17
 {
     public class Program
@@ -6,16 +9,16 @@ namespace HomeWork16_17
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddTransient<IMeetingRoomSettingsService, MeetingRoomSettingsService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -34,3 +37,4 @@ namespace HomeWork16_17
         }
     }
 }
+
