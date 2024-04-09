e415f816bf980c2a9e009166088f5c2125ee9ce6
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:11:08 +0300

    Add ExecutionManager

--- EvgenBy/Home_work11/Home_work11/ExecutionManager.cs
+++ EvgenBy/Home_work11/Home_work11/ExecutionManager.cs
@@ -8,5 +8,19 @@ namespace Home_work11
 {
     internal class ExecutionManager
     {
+        public static void DoInventory(Inventory inventory)
+        {
+            Console.WriteLine("Performing inventory...");
+
+            decimal totalValue = inventory.GetPrice();
+            decimal maxPrice = inventory.GetMaxPrice();
+            decimal minPrice = inventory.GetMinPrice();
+            decimal avgPrice = inventory.GetAvgPrice();
+
+            Console.WriteLine($"Total Inventory Value: {totalValue}");
+            Console.WriteLine($"Maximum Price: {maxPrice}");
+            Console.WriteLine($"Minimum Price: {minPrice}");
+            Console.WriteLine($"Average Price: {avgPrice}");
+        }
     }
 }
