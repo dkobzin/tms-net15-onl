01f034359e29e8d3e700cf58b24feccf06ecd690
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:13:58 +0300

    add a body

--- EvgenBy/Home_work11/Home_work11/Program.cs
+++ EvgenBy/Home_work11/Home_work11/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            
+            Product product1 = new Product(1, "Product 1", 10.99m, 5);
+            Product product2 = new Product(2, "Product 2", 5.99m, 10);
+
+            
+            Inventory inventory = new Inventory();
+
+            
+            inventory.AddProduct(product1);
+            inventory.AddProduct(product2);
+
+            
+            ExecutionManager.DoInventory(inventory);
         }
-    }
 }
