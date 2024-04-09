6958ede968e3773303eeb74f2433e0614208fcfc
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-09 18:54:53 +0300

    add servicess

---
+++ EvgenBy/HomeWork16_17/HomeWork16_17/Services/MeetingRoomSettingsService.cs
@@ -0,0 +1,52 @@
+﻿using HomeWork16_17.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace HomeWork16.Services
+{
+    public class MeetingRoomSettingsService : IMeetingRoomSettingsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public MeetingRoomSettingsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public MeetingRoomSettings GetSettings()
+        {
+
+            return GetSettings(string.Empty);
+        }
+
+        public MeetingRoomSettings GetSettings(string meetingRoomName)
+        {
+            var meetingRoomSettings = _configuration.GetSection($"MeetingRoomSettings:{meetingRoomName}").Get<MeetingRoomSettings>();
+            return meetingRoomSettings;
+        }
+
+        public void SaveSettings(string meetingRoomName, MeetingRoomSettings newSettings)
+        {
+            var json = JsonConvert.SerializeObject(newSettings);
+            var configuration = _configuration as IConfigurationRoot;
+            var configPath = configuration?.FilePath;
+
+            var existingSettings = configuration?.GetSection("MeetingRoomSettings").GetChildren();
+            var newSettingsDict = new Dictionary<string, MeetingRoomSettings>();
+
+            foreach (var section in existingSettings)
+            {
+                var existingSetting = section.Get<MeetingRoomSettings>();
+                newSettingsDict.Add(section.Key, existingSetting);
+            }
+
+            newSettingsDict[meetingRoomName] = newSettings;
+
+            var newJson = JsonConvert.SerializeObject(newSettingsDict, Formatting.Indented);
+            File.WriteAllText(configPath, newJson);
+        }
+    }
+}
