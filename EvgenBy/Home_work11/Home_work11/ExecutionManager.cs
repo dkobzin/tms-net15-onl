22057c59a62d3a8eeebc9c962de0dc8628542882
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-04-10 12:23:11 +0300

    E.krepskiy home work11 (#84)
    Home work 11

---
+++ EvgenBy/Home_work11/Home_work11/ExecutionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work11
+{
+    internal class ExecutionManager
+    {
+        public static void DoInventory(Inventory inventory)
+        {
+            Console.WriteLine("Performing inventory...");
+
+            decimal totalValue = inventory.GetPrice();
+            decimal maxPrice = inventory.GetMaxPrice();
+            decimal minPrice = inventory.GetMinPrice();
+            decimal avgPrice = inventory.GetAvgPrice();
+
+            Console.WriteLine($"Total Inventory Value: {totalValue}");
+            Console.WriteLine($"Maximum Price: {maxPrice}");
+            Console.WriteLine($"Minimum Price: {minPrice}");
+            Console.WriteLine($"Average Price: {avgPrice}");
+        }
+    }
+}
---
+++ EvgenBy/Home_work11/Home_work11/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work11
+{
+    internal class Inventory
+    {
+        private List<Product> products;
+
+        public Inventory()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public decimal GetPrice()
+        {
+            decimal price = 0;
+
+            foreach (var product in products)
+            {
+                price += product.Price * product.Quantity;
+            }
+            return price;
+        }
+
+        public decimal GetMaxPrice()
+        {
+            if (products.Count == 0)
+                return 0;
+
+            return products.Max(p => p.Price);
+        }
+
+        public decimal GetMinPrice()
+        {
+            if (products.Count == 0)
+                return 0;
+
+            return products.Min(p => p.Price);
+        }
+
+        public decimal GetAvgPrice()
+        {
+            if (products.Count == 0)
+                return 0;
+
+            return products.Average(p => p.Price);
+        }
+    }
+}
