15e65bc31a0502ec822ac746e4774fb936c53213
Denis <denis11you777602@gmail.com>
2024-04-10 09:06:05 +0300

    CompletedHomeTask17And18

---
+++ DenisIlimbaev/HomeTask17And18/Models/MainSettingsModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace HomeTask15Web.Models
+{
+    public class Container
+    {
+        public General GeneralSettings { get; set; }
+        public Users UserSettings { get; set; }
+    }
+    public class MainSettingsModel
+    {
+        public Container Container { get; init; }
+        public MainSettingsModel(General general, Users settings)
+        {
+            Container = new Container()
+            {
+                UserSettings = settings,
+                GeneralSettings = general
+            };
+        }
+
+
+    }
+}
---
+++ DenisIlimbaev/HomeTask17And18/Service/SettingsService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using HomeTask15Web.Models;
+namespace HomeTask15Web.Service
+{
+
+    public class SettingsService : ISettingsService
+    {
+        private readonly IConfiguration _configuration;
+        public SettingsService(IConfiguration config)
+        {
+            _configuration = config;
+        }
+        public MainSettingsModel GetMeSettings()
+        {
+            var settings = _configuration.GetSection("GeneralSettings").Get<General>();
+            var userSettings = _configuration.GetSection("UserSettings").Get<Users>();
+            return settings != null & userSettings != null ? new MainSettingsModel(settings!, userSettings!) : throw new NullReferenceException();
+        }
+        public void PostMeSettings(Container set)
+        {
+            string json = JsonSerializer.Serialize(set, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            string path = "appsettings.json";
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.Write(json);
+            }
+
+        }
+    }
+}
