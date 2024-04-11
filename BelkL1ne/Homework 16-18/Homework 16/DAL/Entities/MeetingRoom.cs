554de0fff120dd0f1c5cf5ed950632f2cc5bd9e9
BelkL1ne <156948945+BelkL1ne@users.noreply.github.com>
2024-04-11 08:46:06 +0300

    Homework 16 - 18 (#81)
    * Homework 16 - 18
    
    ---------
    
    Co-authored-by: Dmitry Kobzin <dskobzin@hotmail.com>

---
+++ BelkL1ne/Homework 16-18/Homework 16/DAL/Entities/MeetingRoom.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_16.DAL.Entities
+{
+    public class MeetingRoom
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int MaxPeople { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
