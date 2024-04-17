af501728e20b97a912d2ef2ff4b0d9ee0319aef0
Aleksandr Tkachev <r91tap@gmail.com>
2024-04-17 15:41:52 +0300

    AleksandrTkachevHimeWork16

---
+++ Dobronom/AleksandrHw16/AleksandrHw16/Controllers/MeetingRoomController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AleksandrHw16.Models;
+using System.ComponentModel;
+
+namespace AleksandrHw16.Controllers
+{
+    public class MeetingRoomController : Controller
+    {
+        private readonly MeetingRoomSettingsService _meetingRoomSettingsService;
+        public MeetingRoomController(MeetingRoomSettingsService meetingRoomSettingsService)
+        {
+            _meetingRoomSettingsService = meetingRoomSettingsService;
+        }
+        public IActionResult OutputSettings()
+        {
+            var settings = _meetingRoomSettingsService.GetMeetingRoom();
+            return Ok(settings);
+        }
+
+    }
+}
---
+++ Dobronom/AleksandrHw16/AleksandrHw16/Models/MeetingRoom.cs
@@ -0,0 +1,16 @@
+﻿namespace AleksandrHw16.Models
+{
+    public class MeetingRoom
+    {
+        public DateTime Time { get; set; }
+        public int QuantityPeople { get; set; }
+        public TimeSpan MaxMeetingDuration { get; set; }
+        public MeetingRoom(DateTime time, int quantityPeople, TimeSpan maxMeetingDuration)
+        {
+            Time = time;
+            QuantityPeople = quantityPeople;
+            MaxMeetingDuration = maxMeetingDuration;
+        }
+
+    }
+}
