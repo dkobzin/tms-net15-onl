12efd98d46ba807afe20f05d1ab25e9125c6c160
Dobronom <157412457+Dobronom@users.noreply.github.com>
2024-04-17 17:01:44 +0300

    AleksandrTkachevHimeWork16 (#103)
    * AleksandrTkachevHimeWork16
    
    * Исправил Gitignor и добавил csproj
    
    ---------
    
    Co-authored-by: Dmitry Kobzin <dskobzin@hotmail.com>

---
+++ Dobronom/AleksandrHw16/AleksandrHw16/Models/MeetingRoomSettingsService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Options;
+using System.Runtime;
+
+namespace AleksandrHw16.Models
+{
+    public class MeetingRoomSettingsService
+    {
+        public MeetingRoom GetMeetingRoom()
+        {
+            return new MeetingRoom(new DateTime(2024, 4, 12, 13, 0, 0), 7, new TimeSpan(4, 0, 0));
+        }
+    }
+}
