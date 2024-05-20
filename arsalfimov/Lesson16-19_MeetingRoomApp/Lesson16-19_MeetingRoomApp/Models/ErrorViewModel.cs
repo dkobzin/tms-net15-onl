8daf1897e2a979b9b87bc0d121ae98c1d9991e76
Arsen Alfimov <Alfimov.ars@gmail.com>
2024-05-20 14:07:09 +0300

    add HW16-19 alfimov

---
+++ arsalfimov/Lesson16-19_MeetingRoomApp/Lesson16-19_MeetingRoomApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson16_19_MeetingRoomApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
