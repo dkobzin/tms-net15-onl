be115a53b7efbe55448d9e3d4342c2a06218b495
Dmitry Kobzin <dskobzin@hotmail.com>
2024-05-23 06:17:41 +0300

    Samples to lesson 26

---
+++ Samples/Lesson26/LinqSamples/ApplicationDbContext.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinqSamples;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Pinguin> Pinguins => Set<Pinguin>();
+    public DbSet<LittlePinguin> LittlePinguins => Set<LittlePinguin>();
+    
+
+    protected ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(message => Debug.WriteLine(message));
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pinguin>()
+            .ToTable("penguins");
+        
+        modelBuilder.Entity<Pinguin>()
+            .HasKey(p => p.Id);
+        
+        modelBuilder.Entity<Pinguin>()
+            
+        modelBuilder.Entity<Pinguin>()
+            .Property(p => p.Species);
+        
+        modelBuilder.Entity<Pinguin>()
+            .Property(p => p.Island);
+        
+        modelBuilder.Entity<Pinguin>()
+            .Property(p => p.BillDepthMm);
+        
+        modelBuilder.Entity<Pinguin>()
+            .Property(p => p.BodyMassG);
+
+        modelBuilder.Entity<Pinguin>()
+            .Property(p => p.BillLengthMm);
+        
+        modelBuilder.Entity<Pinguin>()
+            .Property(p => p.FlipperLengthMm);
+
+
+        modelBuilder.Entity<LittlePinguin>()
+            .ToTable("little_penguins");
+
+        modelBuilder.Entity<LittlePinguin>()
+            .HasKey(p => p.Id);
+        
+        modelBuilder.Entity<LittlePinguin>()
+            .Property(p => p.Species);
+        
+        modelBuilder.Entity<LittlePinguin>()
+            .Property(p => p.Island);
+        
+        modelBuilder.Entity<LittlePinguin>()
+            .Property(p => p.BillDepthMm);
+        
+        modelBuilder.Entity<LittlePinguin>()
+            .Property(p => p.BodyMassG);
+
+        modelBuilder.Entity<LittlePinguin>()
+            .Property(p => p.BillLengthMm);
+        
+        modelBuilder.Entity<LittlePinguin>()
+            .Property(p => p.FlipperLengthMm);
+        
+    }
+}
---
+++ Samples/Lesson26/LinqSamples/Pinguin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSamples
+{
+
+    [Table("penguins")]
+    public class Pinguin
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("species")]
+        public string? Species { get; set; }
+        
+        [Column("island")]
+        public string? Island { get; set; }
+        
+        [Column("bill_length_mm")]
+        public float? BillLengthMm { get; set; }
+        
+        [Column("bill_depth_mm")]
+        public float? BillDepthMm { get; set; }
+        
+        [Column("flipper_length_mm")]
+        public float? FlipperLengthMm { get; set; }
+        
+        [Column("body_mass_g")]
+        public float? BodyMassG { get; set; }
+
+        [Column("sex")]
+        public string? Sex { get; set; }
+        /*public int? ChildrenId { get; set; }
+        public LittlePinguin Children { get; set; }*/
+    }
+}
