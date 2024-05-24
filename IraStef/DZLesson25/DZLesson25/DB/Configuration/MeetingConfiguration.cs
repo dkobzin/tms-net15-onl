3d1b73c7927d33fc2d6d145f20e334b4f9e181dc
Ira Stef <iran.glare@gmail.com>
2024-05-24 12:10:48 +0300

    DZLesson25

---
+++ IraStef/DZLesson25/DZLesson25/DB/Configuration/MeetingConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DZLesson25.DB.Configuration
+{
+    public class MeetingConfiguration : IEntityTypeConfiguration<Meeting>
+    {
+        public void Configure(EntityTypeBuilder<Meeting> entity)
+        {
+            entity.HasKey(t => t.Id);
+            entity.HasOne(t => t.Room).WithMany().HasForeignKey(t => t.Id);
+            entity.HasMany(t => t.usersInMeetings).WithOne(t => t.Meeting).HasForeignKey(t => t.IdMeeting);
+        }
+    }
+}
---
+++ IraStef/DZLesson25/DZLesson25/Program.cs
@@ -0,0 +1,41 @@
+
+using DZLesson25.DB;
+using DZLesson25.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace DZLesson25
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<MeetingDbContext>(options => 
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddTransient<IMeetingService, MeetingService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
