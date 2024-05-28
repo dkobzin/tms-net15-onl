51ba576497ec85e7561b601b303db53b9e08ca37
Denisdeparture <126673275+Denisdeparture@users.noreply.github.com>
2024-05-28 16:25:14 +0300

    FirstCommit (#111)
    * FirstCommit
    
    * UpdateApp

---
+++ DenisIlimbaev/HomeTask25And26/WebApiHT25and26/Model/IdentityModel/Person.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApiHT25and26.Model.IdentityModel
+{
+    public class Person : IdentityUser
+    {
+        public string? Telephone { get; set; }
+        public bool IsSharing { get; set; }
+    }
+}
---
+++ DenisIlimbaev/HomeTask25And26/WebApiHT25and26/Model/RegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiHT25and26.Model
+{
+    public class RegisterModel : LoginModel
+    {
+        public string Name { get; set; } = null!;
+        public string? LastName { get; set; }
+        public string Telephone { get; set; } = null!;
+    }
+}
