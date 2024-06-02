191ac134f54eee9e98cb7cba4e35da08cf8dca69
mywwave <mywwave@gmail.com>
2024-06-02 22:05:16 +0300

    add

---
+++ StasKolesnik/HW25/HW25/DB/MeetingRoom.cs
@@ -0,0 +1,8 @@
+﻿namespace DZLesson25.DB
+{
+    public class MeetingRoom
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ StasKolesnik/HW25/HW25/DB/UsersInMeeting.cs
@@ -0,0 +1,11 @@
+﻿namespace DZLesson25.DB
+{
+    public class UsersInMeeting
+    { 
+        public int IdUser { get; set; }
+        public int IdMeeting { get; set; }
+
+        public User? User { get; set; }
+        public Meeting? Meeting { get; set; }
+    }
+}
