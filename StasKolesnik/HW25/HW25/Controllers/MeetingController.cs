916423bf9ee2018f1561c29ada8f950ccd06702c
mywwave <121500276+mywwave@users.noreply.github.com>
2024-06-03 22:05:07 +0300

    add (#113)

---
+++ StasKolesnik/HW25/HW25/Controllers/MeetingController.cs
@@ -0,0 +1,37 @@
+﻿using DZLesson25.DB;
+using DZLesson25.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DZLesson25.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class MeetingController(IMeetingService meetingService) : ControllerBase
+    {
+        [HttpGet("GetMeetingRooms")]
+        public async Task<List<MeetingRoom>> GetMeetingRooms() => await meetingService.GetMeetingRooms();
+        [HttpGet("GetMeetings")]
+        public async Task<List<Meeting>> GetMeetings() => await meetingService.GetMeetings();
+        [HttpGet("GetUsers")]
+        public async Task<List<User>> GetUsers() => await meetingService.GetUsers();
+        [HttpGet("GetUsersInMeeting")]
+        public async Task<List<User>> GetUsersInMeeting(int meetingId) => await meetingService.GetUsersInMeeting(meetingId);
+
+        [HttpPost("CreateMeetingRoom")]
+        public async Task CreateMeetingRoom(MeetingRoom room) => await meetingService.CreateMeetingRoom(room);
+        [HttpPost("CreateMeeting")]
+        public async Task CreateMeeting(MeetingRoom room, string name) => await meetingService.CreateMeeting(room, name);
+        [HttpPost("CreateUser")]
+        public async Task CreateUser(User user) => await meetingService.CreateUser(user);
+        [HttpPost("AddUserToMeeting")]
+        public async Task AddUserToMeeting(int userId, int meetingId) => await meetingService.AddUserToMeeting(userId, meetingId);
+        [HttpPost("EditMeeting")]
+        public async Task EditMeeting(Meeting meeting) => await meetingService.EditMeeting(meeting);
+        [HttpPost("EditUser")]
+        public async Task EditUser(User user) => await meetingService.EditUser(user);
+        [HttpPost("RemoveMeeting")]
+        public async Task RemoveMeeting(int meetingId) => await meetingService.RemoveMeeting(meetingId);
+        [HttpPost("RemoveUserFromMeeting")]
+        public async Task RemoveUserFromMeeting(int meetingId, int userId) => await meetingService.RemoveUserFromMeeting(meetingId, userId);
+    }
+}
---
+++ StasKolesnik/HW25/HW25/Migrations/20240523140618_initial.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DZLesson25.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MeetingRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetingRooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meetings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IdRoom = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Meetings_MeetingRooms_Id",
+                        column: x => x.Id,
+                        principalTable: "MeetingRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersInMetings",
+                columns: table => new
+                {
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    IdMeeting = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersInMetings", x => new { x.IdMeeting, x.IdUser });
+                    table.ForeignKey(
+                        name: "FK_UsersInMetings_Meetings_IdMeeting",
+                        column: x => x.IdMeeting,
+                        principalTable: "Meetings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersInMetings_Users_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersInMetings_IdUser",
+                table: "UsersInMetings",
+                column: "IdUser");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersInMetings");
+
+            migrationBuilder.DropTable(
+                name: "Meetings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "MeetingRooms");
+        }
+    }
+}
