15c4508180c17a57319927e43517bbfc2610ddcd
Dmitry Kobzin <dskobzin@hotmail.com>
2024-06-16 16:43:01 +0300

    Modify  examples for .Net 4.8

--- Samples/Lesson31/WinFormsAppEAP/WinFormsAppEAP/Program.cs
+++ Samples/Lesson31/WinFormsAppEAP/WinFormsAppEAP/Program.cs
@@ -1,17 +1,16 @@
-namespace WinFormsAppEAP
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsAppEAP;
+
+public class Program
 {
-    internal static class Program
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        //ApplicationConfiguration.Initialize();
+        Application.Run(new PrimeNumberCalculatorMain());
     }
 }
